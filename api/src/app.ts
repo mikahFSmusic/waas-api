7d451625747d603796e4e4e08fc336a0e57e9c96
mikahfsmusic <mikahfsmusic@gmail.com>
2023-01-11 04:36:35 -0500

    adds firebase to correct location

--- api/src/app.ts
+++ api/src/app.ts
@@ -6,8 +6,8 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
-import { initializeApp } from 'firebase/app';
-import { getStorage, ref } from 'firebase/storage';
+
+// Routes
 import UploadRoutes from './routes/uploadAudioFile.route';
 
 dotenv.config();
@@ -24,23 +24,6 @@ const PORT: number = parseInt(process.env.PORT as string, 10);
 
 const app = express();
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-};
-
-const firebaseApp = initializeApp(firebaseConfig);
-console.log(firebaseApp);
-
-// Get a reference to the storage service, which is used to create references in your storage bucket
-const storage = getStorage();
-
-// Create a storage reference from our storage service
-const storageRef = ref(storage);
 /**
  *  App Configuration
  */
--- api/src/controllers/uploadAudioFile.controller.ts
+++ api/src/controllers/uploadAudioFile.controller.ts
@@ -1,4 +1,17 @@
 import express, { Request, Response } from 'express';
+import { initializeApp } from 'firebase/app';
+import { getStorage, ref, uploadBytes } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+initializeApp(firebaseConfig);
 
 export const uploadAudioFile = async (req: Request, res: Response) => {
   try {
@@ -7,7 +20,14 @@ export const uploadAudioFile = async (req: Request, res: Response) => {
       return;
     }
 
-    console.log(req.file);
+    const storage = getStorage();
+
+    const storageRef = ref(storage, 'tmp/audio/' + req.file.originalname);
+
+    const uploadResult = await uploadBytes(storageRef, req.file.buffer);
+
+    console.log(uploadResult);
+
     res.send('File uploaded successfully');
   } catch (err) {
     console.log(err);
