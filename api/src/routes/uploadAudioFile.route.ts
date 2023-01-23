b59cbcc755c09940576fe161add5127259b9b4ed
mikahfsmusic <mikahfsmusic@gmail.com>
2023-01-22 23:45:29 -0500

    better syntax for blank request

--- api/src/routes/uploadAudioFile.route.ts
+++ api/src/routes/uploadAudioFile.route.ts
@@ -10,7 +10,7 @@ const UPLOAD_ROUTE = '/upload';
 router.post(UPLOAD_ROUTE, upload.single('file'), uploadAudioFile);
 
 // get request to /upload logs 'Upload route reached'
-router.get(UPLOAD_ROUTE, (req, res) => {
+router.get(UPLOAD_ROUTE, (_, res) => {
   console.log('Upload route reached');
   res.send('Upload route reached');
 });
